5e37ca77bb95d2c31f103b9683bb1d6e8fc991f3
sunr4ze <qwfqfqwqdqdq@gmail.com>
2023-02-19 02:57:06 +0300

    Creating a template for the main page and working with a block for product design

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
